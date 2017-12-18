d4189669451056f9884db32d845ac370db0cbeb4
yabdelmotaleb <yabdelmotaleb@ITEM-S37026.emea.msad.sopra>
2017-12-18 21:44:58 +0000

    ajout aop

---
+++ RestProject/src/main/java/com/labs/aop/AopMonitor.java
@@ -0,0 +1,27 @@
+package com.labs.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AopMonitor {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AopMonitor.class);
+
+	@Before("execution(* com.labs.service.*.*(..))")
+	public void beforeExecution(JoinPoint joinPoint) {
+		LOGGER.info("Before: " + joinPoint);
+	}
+	
+	@After("execution(* com.labs.service.*.*(..))")
+	public void afterExecution(JoinPoint joinPoint) {
+		LOGGER.info("After: " + joinPoint);
+	}
+
+}
