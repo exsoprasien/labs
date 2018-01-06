b0c0c9c785347c3ddc99c0bd0ec617a41f4cc39d
yabdelmotaleb <yabdelmotaleb@ITEM-S37026.emea.msad.sopra>
2018-01-06 11:54:28 +0000

    add userVO

---
+++ RestProject/src/main/java/com/labs/model/UserVo.java
@@ -0,0 +1,57 @@
+package com.labs.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserVo {
+	
+	@JsonCreator
+	public UserVo(@JsonProperty("id") String id, @JsonProperty("firstname") String firstName,
+			@JsonProperty("lastname") String name, @JsonProperty("birthdate") Date birthdate) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.name = name;
+		this.birthdate = birthdate;
+	}
+
+	private String id;
+	private String firstName;
+	private String name;
+	private Date birthdate;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+}
